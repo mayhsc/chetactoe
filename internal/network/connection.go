c47e876aa45ff83e653a4c3574f9139920fdf49b
unpublished3 <chapagainsakshyam@gmail.com>
2026-08-04 17:52:46 +0545

    Filter out local device

--- internal/network/connection.go
+++ internal/network/connection.go
@@ -6,7 +6,7 @@ import (
 	"time"
 )
 
-const port = 1052
+const port = 2000
 const connectionSignal = "HELLO_CHETACTOE"
 
 func DiscoverDevices() {
@@ -24,18 +24,23 @@ func DiscoverDevices() {
 
 	fmt.Println("Listening for other players...")
 	buf := make([]byte, 1024)
+	myIPs := getLocalIP()
 
 	for {
 		n, remoteAddr, err := conn.ReadFromUDP(buf)
 
-		if (err != nil) {
+		if err != nil {
+			continue
+		}
+
+		if myIPs[remoteAddr.IP.String()] {
 			continue
 		}
 
 		message := string(buf[:n])
 
 		if message == connectionSignal {
-			fmt.Printf("Peer discovered at IP: %s", remoteAddr.IP.String())
+			fmt.Printf("Peer discovered at IP: %s\n", remoteAddr.IP.String())
 		}
 	}
 }
@@ -62,4 +67,24 @@ func boradcastPresence() {
 		_, _ = conn.Write([]byte(connectionSignal))
 		time.Sleep(2 * time.Second)
 	}
+}
+
+func getLocalIP() map[string]bool {
+	ips := make(map[string]bool)
+
+	addrs, err := net.InterfaceAddrs()
+
+	if err != nil {
+		return ips
+	}
+
+	for _, addr := range addrs {
+		if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				ips[ipnet.IP.String()] = true
+			}
+		}
+	}
+
+	return ips
 }
