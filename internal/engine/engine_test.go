84bdbdeb5ecd25df12900a5fb5cdb239e134db95
unpublished3 <chapagainsakshyam@gmail.com>
2026-08-08 10:50:36 +0545

    Convert to method

--- internal/engine/engine_test.go
+++ internal/engine/engine_test.go
@@ -91,7 +91,7 @@ func TestIsWinningState(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			actual := isWinningState(tt.board, tt.player)
+			actual := tt.board.isWinningState(tt.player)
 			if actual != tt.expected {
 				t.Errorf("isWinningState() = %v, expected %v", actual, tt.expected)
 			}
--- internal/engine/gameboard.go
+++ internal/engine/gameboard.go
@@ -86,34 +86,3 @@ func (gb *GameBaord) getLabelForPiece(pType PieceType, p Player) string {
 
 	return labels[i][int(pType)]
 }
-
-func isWinningState(bd Board, p Player) bool {
-	pieces := bd.pieces
-	leftDiagonal, rightDiagonal := true, true
-
-	matchesPlayer := func(row, col int) bool {
-		piece := pieces[row][col]
-		return piece != nil && piece.player == p
-	}
-
-	for i := range 4 {
-		if matchesPlayer(i, 0) && matchesPlayer(i, 1) && matchesPlayer(i, 2) && matchesPlayer(i, 3) {
-			return true
-		}
-
-		if matchesPlayer(0, i) && matchesPlayer(1, i) && matchesPlayer(2, i) && matchesPlayer(3, i) {
-			return true
-		}
-
-		if !matchesPlayer(i, i) {
-			leftDiagonal = false
-		}
-
-		if !matchesPlayer(3-i, i) {
-			rightDiagonal = false
-		}
-
-	}
-
-	return leftDiagonal || rightDiagonal
-}
